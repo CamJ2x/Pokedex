97512ab3aad3c7d2f51a88cdb18be5eac5790242
Cameron <cjohnson22@msu.idserve.net>
2022-12-12 09:47:23 -0500

    Initial Commit

---
+++ Challenge3/Challenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge3App.swift
+//  Challenge3
+//
+//  Created by Cameron Johnson on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge3UITests/Challenge3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Challenge3UITestsLaunchTests.swift
+//  Challenge3UITests
+//
+//  Created by Cameron Johnson on 12/12/22.
+//
+
+import XCTest
+
+final class Challenge3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
